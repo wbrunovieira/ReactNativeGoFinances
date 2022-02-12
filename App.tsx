0fb2962f94758e61c71145fdfcf8b036038615eb
wbrunovieira <wbrunovieira77@gmail.com>
2022-02-12 21:50:44 +0000

    added fonts in themes, added raect-native-responsive-fontsize library, started the Header Component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider  } from 'styled-components'
-import themes from './src/global/styles/themes';
+
+import { 
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+
+ } from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/themes';
 import { Dashboard } from './src/screens/Dashboard';
@@ -9,6 +17,15 @@ import { Dashboard } from './src/screens/Dashboard';
 
 
 export default function App() {
+  const [ fontsloaded ] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsloaded) {
+    return <AppLoading />
+  }
   return (
 
     <ThemeProvider theme={theme}>
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
 
-import { Container , Title} from './styles';
+import { 
+    Container ,
+    Header,
+    UserInfo,
+    UserWrapper,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+} from './styles';
 
 export function Dashboard() {
     return (<Container>
-                <Title>Dashboard</Title>
+               <Header>
+                <UserWrapper>
+                       <UserInfo>
+                       <Photo 
+                       source={{uri:'https://avatars.githubusercontent.com/u/68607000?v=4'}}
+                       />
+                       <User>
+                           <UserGreeting>Ola,</UserGreeting>
+                           <UserName>Bruno</UserName>
+                       </User>
+                   </UserInfo>
+                 </UserWrapper>
+               </Header>
             </Container>
     )
 }
