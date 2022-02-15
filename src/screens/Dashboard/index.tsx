c3fd1ef6b6d753465ad3755fe073c491c343d904
wbrunovieira <wbrunovieira77@gmail.com>
2022-02-15 20:52:16 +0000

    created the TransationsCards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransationCard , TransationCardProps} from '../../components/TransationCard';
 
 
 
@@ -13,10 +15,58 @@ import {
     UserGreeting,
     UserName,
     Icons,
-    HighlightCards
+    HighlightCards,
+    Transations,
+    Title,
+    TransationList
+
 } from './styles';
 
+export interface DataListProps extends TransationCardProps {
+    id: string;
+}
+
 export function Dashboard() {
+
+    
+
+    const data: DataListProps[] = [
+        
+        {
+        id:'1', 
+        type: "positive",
+        title:"Desenvolvimento de sites",
+        amount:"R$ 12.000,00",
+        category:{
+        name:"Vendas",
+        icon:'dollar-sign'
+     },
+    date:"13/04/2022"
+ },
+        {
+        id:'2',
+        type: "negative",
+        title:"Hamburgue Pixx",
+        amount:"R$ 20,00",
+        category:{
+        name:"Alimentacao",
+        icon:'coffee'
+     },
+    date:"13/04/2022"
+ },
+        {
+        id:'3 ',
+        type: "negative",
+        title:"Aluguel do Apartamento",
+        amount:"R$ 1.200,00",
+        category:{
+        name:"Casa",
+        icon:'shopping-bag'
+     },
+    date:"24/04/2022"
+ },
+];
+
     return (<Container>
                <Header>
                 <UserWrapper>
@@ -38,6 +88,16 @@ export function Dashboard() {
                     <HighlightCard  type="total" title="Total" amount="R$ 78.200,00" lastTransaction=" 05 de abril de 2022"/>
                     
                </HighlightCards>
+
+               <Transations>
+                   <Title>Listagem</Title>
+
+                   <TransationList 
+                        data={data}
+                        keyExtractor={item => item.id }
+                        renderItem={({ item }) =><TransationCard data={item} /> }
+                   /> 
+               </Transations>
             </Container>
     )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,11 +1,13 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { Feather } from '@expo/vector-icons'
 import {  RFPercentage, RFValue } from 'react-native-responsive-fontsize';
-import {  getStatusBarHeight } from 'react-native-iphone-x-helper';
+import {  getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { DataListProps } from '.';
 
 export const Container = styled.View`
     flex: 1;
-    
     background-color: ${( {theme}) => theme.colors.background};
     
 `
@@ -13,11 +15,11 @@ export const Container = styled.View`
 export const Header = styled.View`
     width: 100%;
     height: ${RFPercentage(42)}px;
-
+    
     background-color: ${({theme}) => theme.colors.primary};
-
+    
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
     flex-direction: row;
     
 `
@@ -26,7 +28,7 @@ export const UserWrapper = styled.View`
 width: 100%;
 padding: 0 24px;
 
-
+margin-top: ${getStatusBarHeight() + RFValue(28)}px;
 flex-direction: row;
 justify-content: space-between;
 align-items: center;
@@ -75,6 +77,27 @@ export const HighlightCards = styled.ScrollView.attrs({
  width: 100%;
 
  position: absolute;
- margin-top: ${RFValue(190)}px;
+ margin-top: ${RFValue(170)}px;
 `
+export const Transations = styled.View`
+    flex: 1;
+    padding: 0 24px;
 
+    margin-top: ${RFPercentage(18)}px;
+`;
+
+export const Title = styled.Text`
+    font-size: ${RFValue(18)}px;
+    font-family: ${({theme}) => theme.fonts.regular};
+
+    margin-bottom: 16px;
+    `;
+
+export const TransationList = styled(
+    FlatList as new () => FlatList<DataListProps>
+    ).attrs({
+    showsVerticalScrollIndicator:false,
+    contentContainerStyle:{
+      paddingBottom: getBottomSpace()
+ } 
+})``;
