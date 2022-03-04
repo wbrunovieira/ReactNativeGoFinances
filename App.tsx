b83b9cca0969c386b5c175f7f06b9c0e23965939
wbrunovieira <wbrunovieira77@gmail.com>
2022-03-04 20:43:06 +0000

    added react navigation

--- App.tsx
+++ App.tsx
@@ -12,9 +12,9 @@ import {
 
 import theme from './src/global/styles/themes';
 
-import { Register } from './src/screens/Register';
+import { NavigationContainer } from '@react-navigation/native';
 
-
+import { AppRoutes } from './src/routes/app.routes'
 
 
 export default function App() {
@@ -30,8 +30,10 @@ export default function App() {
   return (
 
     <ThemeProvider theme={theme}>
+      <NavigationContainer>
 
-      <Register />
+           <AppRoutes />
+      </NavigationContainer>
  
     </ThemeProvider>
    
